3fba5d034e7dd4c4829b0564ab8d25000d863ada
Ol1mowski <olimarki123@wp.pl>
2025-07-24 11:47:22 +0200

    feat: add config files

---
+++ server/src/test/java/maile/com/example/mailer/MailerApplicationTests.java
@@ -0,0 +1,13 @@
+package maile.com.example.mailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
